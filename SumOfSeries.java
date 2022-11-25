1da19ffe4fbd09941b0f8efb4c43a50efbf9762e
Xavier Rolex <Xavierrolex7@gmail.com>
2022-11-26 01:08:56 +0600

    Update and rename Q3.java to SumOfSeries.java
    Sum of Series
    1/3+1/9+....1/3^n

--- SumOfSeries.java
+++ SumOfSeries.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q3 {
+public class SumOfSeries {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of n: ");
