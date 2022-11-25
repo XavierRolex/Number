54c461040abf706833262d1addb305edff2a7049
Xavier Rolex <Xavierrolex7@gmail.com>
2022-11-26 01:05:13 +0600

    Update and rename Q2.java to a^b.java

--- a^b.java
+++ a^b.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q2 {
+public class a^b {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the 'Base' & 'Power' :");
